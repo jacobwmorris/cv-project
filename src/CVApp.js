e5b9c4e9150cdb5960ab51dfc299bae9c11f584d
jacobwmorris <jacobwmorris0@gmail.com>
2023-05-01 16:10:23 -0400

    Rewrite CVApp as a functional component

--- src/CVApp.js
+++ src/CVApp.js
@@ -1,8 +1,132 @@
-import { Component } from 'react'
+import { Component, useState } from 'react'
 import EditView from './components/EditView'
 import DisplayView from './components/DisplayView'
 
-class CVApp extends Component {
+function CVApp() {
+  const [view, setView] = useState("edit")
+  const [general, setGeneral] = useState({})
+  const [education, setEducation] = useState([])
+  const [nextEducationId, setNextEducationId] = useState(0)
+  const [employment, setEmployment] = useState([])
+  const [nextEmploymentId, setNextEmploymentId] = useState(0)
+
+  //Callbacks
+  function handleSubmit(event) {
+    event.preventDefault()
+    const submitter = event.nativeEvent.submitter
+    const data = new FormData(event.target)
+    if (submitter.hasAttribute("data-btn-done")) {
+      doneEditing(data, setView, setGeneral, setEducation, setEmployment)
+      return
+    }
+    if (submitter.hasAttribute("data-btn-addedu")) {
+      addEducationEntry(data, submitter.getAttribute("data-index"), setEducation)
+      return
+    }
+    if (submitter.hasAttribute("data-btn-rmedu")) {
+      removeEducationEntry(data, submitter.getAttribute("data-index"), setEducation)
+      return
+    }
+    if (submitter.hasAttribute("data-btn-addemp")) {
+      addEmploymentEntry(data, submitter.getAttribute("data-index"), setEmployment)
+      return
+    }
+    if (submitter.hasAttribute("data-btn-rmemp")) {
+      removeEmploymentEntry(data, submitter.getAttribute("data-index"), setEmployment)
+      return
+    }
+  }
+
+  function handleEdit(event) {
+    setView("edit")
+  }
+
+  //Rendering
+  if (view === "edit") {
+    return (
+      <EditView
+      generalContent={general}
+      educationContent={education}
+      employmentContent={employment}
+      handleSubmit={handleSubmit}/>
+    )
+  }
+  return (
+    <DisplayView
+    generalContent={general}
+    educationContent={education}
+    employmentContent={employment}
+    handleEdit={handleEdit}/>
+  )
+}
+
+//Helper functions
+function doneEditing(data, setView, setGeneral, setEducation, setEmployment) {
+  const newGeneral = readGeneralContent(data)
+  const newEducation = readEducationContent(data)
+  const newEmployment = readEmploymentContent(data)
+  setView("display")
+  setGeneral(newGeneral)
+  setEducation(newEducation)
+  setEmployment(newEmployment)
+}
+
+function addEducationEntry(data, index, setEducation) {
+  const newEducation = readEducationContent(data)
+  newEducation.splice(index, 0, {school: "", title: "", start: "", end: ""})
+  setEducation(newEducation)
+}
+
+function removeEducationEntry(data, index, setEducation) {
+  const newEducation = readEducationContent(data)
+  newEducation.splice(index, 1)
+  setEducation(newEducation)
+}
+
+function addEmploymentEntry(data, index, setEmployment) {
+  const newEmployment = readEmploymentContent(data)
+  newEmployment.splice(index, 0, {company: "", position: "", description: "", start: "", end: ""})
+  setEmployment(newEmployment)
+}
+
+function removeEmploymentEntry(data, index, setEmployment) {
+  const newEmployment = readEmploymentContent(data)
+  newEmployment.splice(index, 1)
+  setEmployment(newEmployment)
+}
+
+//Form data functions
+function readGeneralContent(data) {
+  const general = {}
+  general.name = data.get("name")
+  general.email = data.get("email")
+  general.phone = data.get("phone")
+  general.summary = data.get("summary")
+  return general
+}
+
+function readEducationContent(data) {
+  const schools = data.getAll("ed_school")
+  const titles = data.getAll("ed_title")
+  const starts = data.getAll("ed_start")
+  const ends = data.getAll("ed_end")
+  return schools.map((s, i) => {
+    return {school: s, title: titles[i], start: starts[i], end: ends[i]}
+  })
+}
+
+function readEmploymentContent(data) {
+  const companies = data.getAll("em_company")
+  const positions = data.getAll("em_position")
+  const descriptions = data.getAll("em_description")
+  const starts = data.getAll("em_start")
+  const ends = data.getAll("em_end")
+  return companies.map((c, i) => {
+    return {company: c, position: positions[i], description: descriptions[i], start: starts[i], end: ends[i]}
+  })
+}
+
+class CVAppClass extends Component {
   constructor(props) {
     super(props)
 
